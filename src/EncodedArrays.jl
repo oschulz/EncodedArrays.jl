4445e701bd3365481784cba91439ac21ad30f8df
Oliver Schulz <oschulz@mpp.mpg.de>
2026-07-10 18:55:24 +0200

    Remove unused StructArrays dependency and obsolete __precompile__
    Created by generative AI.

--- src/EncodedArrays.jl
+++ src/EncodedArrays.jl
@@ -1,14 +1,11 @@
 # This file is a part of EncodedArrays.jl, licensed under the MIT License (MIT).
 
-__precompile__(true)
-
 module EncodedArrays
 
 using ArraysOfArrays
 using ArraysOfArrays: NestedArrayStyle
 using BitOperations
 using FillArrays
-using StructArrays
 
 include("encoded_array.jl")
 include("varlen_io.jl")
